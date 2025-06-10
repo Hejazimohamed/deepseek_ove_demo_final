b4ae82295dd2c02ab590754dc4174ac0632fda28
mohamedhejazi <hejazi.mohamed@gmail.com>
2025-06-10 05:45:14 -0700

    Remove BOM from mypy.ini, exclude zip/, update CI

---
+++ __init__.py
@@ -0,0 +1,4 @@
+﻿# __init__.py (محتوى اختياري)
+from .ocr_parallel_ui import *  # لتصدير كل الدوال/الكلاسات
+from images_fpr_test import ocr_parallel_ui  # صحيح مع وجود __init__.py
+__all__ = ['func1', 'func2']   # تحديد ما يُصدر عند استيراد الحزمة
